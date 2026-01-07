f9e1b577fed5a4e697eb030b9aac68f11e40733e
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-07 09:39:39 +0300

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+import favicon from "$lib/assets/favicon.svg";
+import "./layout.css";
+import { ModeWatcher } from "mode-watcher";
 
-	let { children } = $props();
+let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon}>
+</svelte:head>
+
+<ModeWatcher />
+
+<main class="content-center min-h-dvh bg-background">{@render children()}</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<form class="flex flex-col gap-2 py-3 px-5 mx-auto max-w-3xl rounded-3xl bg-muted">
+	<textarea
+		class="w-full border-0 resize-none min-h-12"
+		placeholder="Ask anything..."
+	></textarea>
+
+	<div class="flex justify-between">
+		<button class="btn btn-primary">File</button>
+		<button class="btn btn-primary">Send</button>
+	</div>
+</form>

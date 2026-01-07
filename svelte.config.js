a34e5d953954d8e8b2aebbd091c6a91ebe0c4ec2
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-07 12:18:16 +0300

    enable experimental svelte

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,14 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		experimental: { remoteFunctions: true },
+	},
+
+	compilerOptions: {
+		experimental: { async: true },
+	},
 };
 
 export default config;

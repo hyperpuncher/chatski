93a6b0a40301e48437d5c5c5bf8596be5e75b856
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-12 15:22:21 +0300

    remove memoryStorage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -4,5 +4,3 @@ import localStorageDriver from "unstorage/drivers/localstorage";
 export const localStorage = createStorage({
 	driver: localStorageDriver({ base: "chat" }),
 });
-
-export const memoryStorage = createStorage();

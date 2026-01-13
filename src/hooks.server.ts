25164b4f1993fbbd4c8301ca30bba8824e68f85b
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-13 23:11:27 +0300

    add headers hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -31,4 +31,17 @@ const sessionHandler: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-export const handle = sequence(authHandler, sessionHandler);
+const headersHandler: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event);
+	response.headers.set(
+		"Strict-Transport-Security",
+		"max-age=63072000; includeSubDomains; preload",
+	);
+	response.headers.set("X-Frame-Options", "DENY");
+	response.headers.set("X-XSS-Protection", "0");
+	response.headers.set("X-Content-Type-Options", "nosniff");
+	response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+	return response;
+};
+
+export const handle = sequence(authHandler, sessionHandler, headersHandler);

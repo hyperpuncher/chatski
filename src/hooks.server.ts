9431a71ed8b51bc415ca6f72cd3753af6d3ae3bc
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-13 23:05:50 +0300

    add email allowlist

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,11 @@
-import { type Handle, redirect } from "@sveltejs/kit";
+import { error, type Handle, redirect } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from "$app/environment";
+import { env } from "$env/dynamic/private";
 import { auth } from "$lib/server/auth";
+
+const AUTH_ALLOWED_EMAILS = env.AUTH_ALLOWED_EMAILS.split(",");
 
 const authHandler: Handle = async ({ event, resolve }) => {
 	return svelteKitHandler({ event, resolve, auth, building });
@@ -14,6 +17,9 @@ const sessionHandler: Handle = async ({ event, resolve }) => {
 	});
 
 	if (session) {
+		if (!AUTH_ALLOWED_EMAILS.includes(session.user.email)) {
+			error(403, "Forbidden");
+		}
 		event.locals.session = session.session;
 		event.locals.user = session.user;
 	}

19653f2ad411d8a558684448f227d4a8cbdbf9de
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-01-18 07:58:00 +0300

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
@@ -8,6 +9,7 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+		SvelteKitPWA(),
 		devtoolsJson(),
 		visualizer({
 			filename: "stats.html",

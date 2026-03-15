164e2aaf20d2be71967828885896c8365241202d
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2026-03-15 09:02:34 +0300

    style: change selection colors to black and white

--- src/lib/components/ai-message.svelte
+++ src/lib/components/ai-message.svelte
@@ -25,7 +25,7 @@ let { content, isStreaming } = $props();
 		},
 		code: {
 			container: "bg-[#171717]",
-			pre: "bg-[#171717] sm:text-base text-primary-foreground dark:text-foreground no-scrollbar",
+			pre: "bg-[#171717] sm:text-base text-primary-foreground dark:text-foreground no-scrollbar selection:bg-neutral-50 selection:text-neutral-950",
 		},
 	}}
 />
